133d0e09fb30ff75f23be23fceabfd84b97cec15
egbejob680 <jobegbe39@gmail.com>
2021-11-29 15:58:45 +0100

    insertions

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ def Prompt():
     print("====================")
     print(" 0 is to quit\n 1 is to add mail\n 2 is to show mail \n 3 is to update mail\n 4 is to delete ")
     print("====================")
-Prompt()
+    output = input("enter a value from 0 - 4: ")
+    if output == "0": 
+        quit()
+    elif output == "1":
+        addMail()
+    elif output == "2":
+        showMail()
+    elif output == "3":
+        updateMail()
+    elif output == "4":
+        deleteMail()
+    else:
+        print("please enter correct value")
 
 def addMail(name,email):
     "modify"
 def showMail():
-def deleteMail():
+    "modify"
 def updateMail():
-def quitMail():
+    "modify"
+
+def deleteMail():
+    "modify"
+def quit():
+    "modify"
+Prompt()
