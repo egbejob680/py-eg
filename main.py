8d1ed816a7ed17284bfc8feca80944bb423cd2e5
egbejob680 <jobegbe39@gmail.com>
2021-11-29 15:38:59 +0100

    added functions

---
+++ main.py
@@ -0,0 +1,13 @@
+def Prompt():
+    display = { "0","1","2","3","4"}
+    print("====================")
+    print(" 0 is to quit\n 1 is to add mail\n 2 is to show mail \n 3 is to update mail\n 4 is to delete ")
+    print("====================")
+Prompt()
+
+def addMail(name,email):
+    "modify"
+def showMail():
+def deleteMail():
+def updateMail():
+def quitMail():
